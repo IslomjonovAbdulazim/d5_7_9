15e2ec51ffdfad9dce98a235055411b4ccabbff5
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-26 17:24:08 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/azimislom/d5_7_9/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.azimislom.d5_7_9
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/new_note_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class NewNotePage extends StatefulWidget {
+  const NewNotePage({super.key});
+
+  @override
+  State<NewNotePage> createState() => _NewNotePageState();
+}
+
+class _NewNotePageState extends State<NewNotePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
