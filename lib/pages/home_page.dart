8111c76016ec62dff183ccf3af063228a8d1c34b
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-26 17:51:36 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:d5_7_9/pages/new_note_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,6 +18,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Notes"),
+        actions: [
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NewNotePage(),
+                ),
+              );
+            },
+            child: Icon(Icons.add, size: 30),
+          ),
+          SizedBox(width: 10),
+        ],
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
--- lib/pages/new_note_page.dart
+++ lib/pages/new_note_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NewNotePage extends StatefulWidget {
   const NewNotePage({super.key});
@@ -8,6 +10,8 @@ class NewNotePage extends StatefulWidget {
 }
 
 class _NewNotePageState extends State<NewNotePage> {
+  TextEditingController noteController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -16,11 +20,33 @@ class _NewNotePageState extends State<NewNotePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("Note Details")),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
           child: Column(
-            children: [],
+            children: [
+              TextField(
+                controller: noteController,
+                decoration: InputDecoration(
+                  hintText: "Note...",
+                ),
+              ),
+              SizedBox(height: 20),
+              CupertinoButton(
+                color: Colors.yellow,
+                onPressed: () async {
+                  final db = await SharedPreferences.getInstance();
+                  List notes = db.getStringList("notes") ?? [];
+                  String note = noteController.text.trim();
+                  if (note.isEmpty) return;
+                  notes.add(note);
+                  await db.setStringList("notes", List<String>.from(notes));
+                  Navigator.pop(context);
+                },
+                child: Center(child: Text("Save")),
+              ),
+            ],
           ),
         ),
       ),
